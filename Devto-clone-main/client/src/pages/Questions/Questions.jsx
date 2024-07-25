98138ecb72e7897d17a43a965ac1e12d4d384f11
Subalakshmi04 <162732329+Subalakshmi04@users.noreply.github.com>
2024-07-26 00:53:29 +0530

    question-page

--- Devto-clone-main/client/src/pages/Questions/Questions.jsx
+++ Devto-clone-main/client/src/pages/Questions/Questions.jsx
@@ -1,7 +1,46 @@
-import './Questions.css';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./Questions.css";
 
-export default function Questions(){
-    return (
-        <h1>Questions Page</h1>
-    )
-}
+const Questions = () => {
+  const { challengeId } = useParams();
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/questions/challenge/${challengeId}`
+        );
+        setQuestions(response.data);
+      } catch (error) {
+        console.error("Error fetching questions:", error);
+      }
+    };
+
+    fetchQuestions();
+  }, [challengeId]);
+
+  return (
+    <div>
+      <h1>Questions</h1>
+        {questions.map((question) => (
+          <div key={question._id} className="question-container">
+            <div className="question">
+                Question:{question.question}
+             </div>
+             <div className="options">
+                Options:
+                <span>A: {question.option1}</span>
+                <span>B: {question.option2}</span>
+                <span>C: {question.option3}</span>
+                <span>D: {question.option4}</span>
+             </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Questions;
